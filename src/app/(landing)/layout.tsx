0c1e1d5b80b18a45e9a0e0891d6ffc82b6a8c435
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-11 22:26:07 +0100

    file setup

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Prop = {}
+
+const LandingPageLayout = ({ children}: {children: React.ReactNode}) => {
+
+    return <div className="flex flex-col container relative">{children}</div>
+}
+
+export default LandingPageLayout
