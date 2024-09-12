8a4cba81118491879c2cd319b55a215ad478745b
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-09-12 11:14:34 +0100

    implement landing, authentication and actions

--- src/app/(landing)/_components/navbar/menu.tsx
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -14,7 +14,7 @@ const Menu = ({ orientation }: MenuProps) => {
   const { section, onSetSection } = useNavigation()
   switch (orientation) {
     case "desktop":
-      return (
+      return ( 
         <Card className="bg-themeGray border-themeGray bg-clip-padding backdrop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl">
           <CardContent className="p-0 flex gap-2">
             {INSTRUCTLY_CONSTANTS.landingPageMenu.map((menuItem) => (
--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import LandingPageNavbar from './_components/navbar';
 
 type Prop = {}
 
 const LandingPageLayout = ({ children}: {children: React.ReactNode}) => {
 
-    return <div className="flex flex-col container relative">{children}</div>
+    return <div className="flex flex-col container relative">
+        <LandingPageNavbar></LandingPageNavbar>
+        {children}</div>
 }
 
 export default LandingPageLayout
